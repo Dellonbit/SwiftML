56879ebd9f5da488c9f1dee42d5f6429122a2af7
Richard Della <gtchdella@gmail.com>
2016-04-25 10:43:31 -0600

    Initial Commit

---
+++ SwiftML/SwiftML.h
@@ -0,0 +1,19 @@
+//
+//  SwiftML.h
+//  SwiftML
+//
+//  Created by arianne on 2016-04-25.
+//  Copyright © 2016 della. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftML.
+FOUNDATION_EXPORT double SwiftMLVersionNumber;
+
+//! Project version string for SwiftML.
+FOUNDATION_EXPORT const unsigned char SwiftMLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftML/PublicHeader.h>
+
+
