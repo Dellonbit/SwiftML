7cf8d6f9c931653ba37d0c75803caaeceb8e180e
Richard Della <gtchdella@gmail.com>
2016-05-11 19:14:25 -0600

    chore: Add logistic Regression algorithm

---
+++ SwiftML/LogReg.swift
@@ -0,0 +1,16 @@
+//
+//  LogReg.swift
+//  SwiftML
+//
+//  Created by arianne on 2016-05-11.
+//  Copyright © 2016 della. All rights reserved.
+//
+
+import Foundation
+class LogReg {
+   
+    init () {
+    
+    }
+
+}
