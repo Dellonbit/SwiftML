d4af78adee18e3b8003e6a521c83948a0f9d8806
Richard Della <gtchdella@gmail.com>
2016-05-12 23:49:21 -0600

    chore: Add svm algorithm

---
+++ SwiftML/Svm.swift
@@ -0,0 +1,14 @@
+//
+//  Svm.swift
+//  SwiftML
+//
+//  Created by arianne on 2016-05-12.
+//  Copyright © 2016 della. All rights reserved.
+//
+
+import Foundation
+class Svm {
+
+    init () {
+    }
+}
